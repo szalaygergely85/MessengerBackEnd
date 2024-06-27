57114bb4a7cd7cff325a020c508e00d1eb9facc2
Gergely Szalay <95410113+gergelyszalay@users.noreply.github.com>
2024-06-27 15:43:19 +0200

    Setting up for tomcat

--- src/main/java/com/gege/ideas/messenger/MessengerApplication.java
+++ src/main/java/com/gege/ideas/messenger/MessengerApplication.java
@@ -2,9 +2,15 @@ package com.gege.ideas.messenger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MessengerApplication {
+public class MessengerApplication extends SpringBootServletInitializer {
+   @Override
+   protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+      return builder.sources(MessengerApplication.class);
+   }
 
    public static void main(String[] args) {
       SpringApplication.run(MessengerApplication.class, args);
